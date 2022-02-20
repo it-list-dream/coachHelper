c895356abcc748226261312378f98cbafcd7f5fa
lj <luo_jun123123@163.com>
2022-02-21 00:07:14 +0800

    改进

---
+++ pages/referenceData/referenceData.js
@@ -0,0 +1,77 @@
+// pages/referenceData/referenceData.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [
+      {value: 'USA', name: '美国'},
+      {value: 'CHN', name: '中国', checked: 'true'},
+      {value: 'BRA', name: '巴西'},
+      {value: 'JPN', name: '日本'},
+      {value: 'ENG', name: '英国'},
+      {value: 'FRA', name: '法国'}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  //复选框
+  checkboxChange(){
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
